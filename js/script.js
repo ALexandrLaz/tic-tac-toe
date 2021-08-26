b6a27c524b9a01dc70e528a8e10cb5951e4748ab
Alexandr Lazar <lazarforcode@gmail.com>
2021-08-26 22:30:23 +0300

    added dynamics of game creation

--- js/script.js
+++ js/script.js
@@ -1,46 +1,119 @@
 "use strict"
-let allBox = document.querySelector("#allBox");
-allBox.addEventListener("click", tictactoe);
+const allBox = document.querySelector("#allBox");
+let move = 4; // Поскольку изначально поле 4 х 4
+let arrWin = [];
+winCombin(move)
+fieldSet(move);
 
-let arrWin = [
-  [0, 1, 2, 3],
-  [4, 5, 6, 7],
-  [8, 9, 10, 11],
-  [12, 13, 14, 15],
-  [0, 4, 8, 12],
-  [1, 5, 9, 13],
-  [2, 6, 10, 14],
-  [3, 7, 11, 15],
-  [0, 5, 10, 15],
-  [3, 6, 9, 12]
-]
+function winCombin(move) {
+  let win = [];
+  let index = 0;
+  for (let i = 0; i < move; i++) { // цикл строки
+    let arr = [];
+    for (let j = 0; j < move; j++) {
+      arr.push(index);
+      index++;
+    }
+    win.push(arr);
+    arr = [];
+  }
+  let step2 = []; // цикл столбы
+  for (let i = 0; i < win.length; i++) {
+    let arr = [];
+    for (let j = 0; j < win[i].length; j++) {
+      arr.push(win[j][i]);
+    }
+    step2.push(arr);
+    arr = [];
+  }
+  let step3 = []; // цикл главная
+  for (let i = 0; i < win.length; i++) {
+    for (let j = i; j < win[i].length; j++) {
+      if (j == i) {
+        step3.push(win[i][j]);
+      }
+    }
+  }
+  let step4 = [].concat(win).reverse(); // цикл побочная
+  let step5 = [];
+  for (let i = 0; i < step4.length; i++) {
+    for (let j = i; j < step4[i].length; j++) {
+      if (j == i) {
+        step5.push(step4[i][j]);
+      }
+    }
+  }
+  win = win.concat(step2);
+  win.push(step3, step5.reverse());
+  arrWin = win;
+  return win
+}
+
+document.querySelectorAll("button").forEach(item => item.addEventListener("click", e => {
+  removeElem(allBox);
+  fieldSet(e.target.value);
+  move = e.target.value;
+}))
+
+//Функция изменения размера поля в зависимости от игры
+function fieldSet(fieldParam) {
+  allBox.style.setProperty('--main-field-width', `${50 * fieldParam}px`)
+  for (let i = 0; i < fieldParam ** 2; i++) {
+    allBox.append(createElem("div", "oneBox", "click", tictactoe));
+  }
+}
+
+function createElem(name, className, event, fn) {
+  let element = document.createElement(name);
+  if (className != undefined) {
+    element.className = className;
+  }
+  if (event != undefined && fn != undefined) {
+    element.addEventListener(event, fn);
+  }
+  return element;
+}
+
+function removeElem(parent) {
+  for (let i = 0; i < parent.children.length; i++) {
+    parent.removeChild(parent.children[i]);
+    i--;
+  }
+}
 
 let prev = "";
 function tictactoe(e) {
-  console.log(e.target);
-  if (prev == "" && e.target.innerHTML == "" || prev == "Нолики") {
+  if (prev == "" && e.target.innerHTML == "") {
     e.target.innerHTML = "X";
-    prev = "Крестики";
-
-  } else if (prev == "Крестики" && e.target.innerHTML == "") {
+    prev = "X";
+  } else if (prev == "X" && e.target.innerHTML == "") {
     e.target.innerHTML = "O";
-    prev = "Нолики";
+    prev = "O";
+  } else if (prev = "O" && e.target.innerHTML == "") {
+    e.target.innerHTML = "X";
+    prev = "X";
   }
-  checkWin(prev);
+  checkWin(prev, move);
 }
 
-function checkWin(step) {
-  for(let i = 0; i < arrWin.length; i++){
+function checkWin(step, move) {
+  arrWin = winCombin(move);
+  for (let i = 0; i < arrWin.length; i++) {
     let repeat = 0;
-    for(let j = 0; j < arrWin[i].length; j++){
-      if(allBox.children[arrWin[i][j]].innerHTML == step){
+    for (let j = 0; j < arrWin[i].length; j++) {
+      if (allBox.children[arrWin[i][j]].innerHTML == step) {
         repeat++;
-        if(repeat == 4){
+        if (repeat == +move) {
           document.getElementsByTagName("h3")[0].innerHTML = `The winner is ${step}`;
-          document.getElementsByClassName("modal")[0].style.display = "block";
+          document.querySelector(".modal").style.display = "block";
         }
-        //вызываем модальное окно
       }
     }
   }
 }
+document.querySelector(".close").addEventListener("click", () => {
+  document.querySelector(".modal").style.display = "none";
+  removeElem(allBox);
+  fieldSet(move)
+  prev = "";
+})
